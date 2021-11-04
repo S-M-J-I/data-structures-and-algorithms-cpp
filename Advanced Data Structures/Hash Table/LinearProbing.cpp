850ae13b449f7ec709a5b0b9fb9938489e25acad
S-M-J-I <jishanlion@gmail.com>
2021-11-04 12:50:58 +0600

    Fixed Linear Probing

--- Advanced Data Structures/Hash Table/LinearProbing.cpp
+++ Advanced Data Structures/Hash Table/LinearProbing.cpp
@@ -1,41 +1,102 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define SIZE 10
 
 using namespace std;
 
 int hashFunction(int key)
 {
-    return key%10;
+    return key % SIZE;
 }
 
-void Insert(int hashTable[], int n, int data)
+int probe(int hashTable[], int key)
+{
+    int index = hashFunction(key);
+    int i = 0;
+    while (hashTable[(index + i) % SIZE] != 0)
+    {
+        i++;
+    }
+
+    return (index + i) % SIZE;
+}
+
+void Insert(int hashTable[], int data)
 {
     int index = hashFunction(data);
-    while(hashTable[index] != 0)
+    if (hashTable[index] != 0)
     {
-        index = (index == n) ? 0 : (index+1 % 10);
+        index = probe(hashTable, data);
     }
     hashTable[index] = data;
 }
 
+int Search(int hashTable[], int key)
+{
+    int index = hashFunction(key);
+    int lastVal = (index == 0 ? SIZE - 1 : index - 1);
+
+    if (hashTable[index] == key)
+    {
+        return index;
+    }
+
+    do
+    {
+        if (index == lastVal)
+        {
+            return -1;
+        }
+
+        index = (index + 1) % SIZE;
+
+    } while (hashTable[index] != key);
+
+    return index;
+}
+
+int Search2(int hashTable[], int key)
+{
+    int index = hashFunction(key);
+    int lastVal = (index == 0 ? SIZE - 1 : index - 1);
+
+    int i = 0;
+    while (hashTable[(index + i) % SIZE] != key)
+    {
+        if((index + i) % SIZE == lastVal) {
+            return -1;
+        }
+        
+        i++;
+    }
+
+    return (index + i) % SIZE;
+}
+
 int main()
 {
-    int hashTable[10];
-    for(int i=0; i<10; i++)
+    int hashTable[SIZE];
+    for (int i = 0; i < SIZE; i++)
     {
         hashTable[i] = 0;
     }
 
-    Insert(&hashTable[hashFunction(26)], 10, 26);
-    Insert(&hashTable[hashFunction(30)], 10, 30);
-    Insert(&hashTable[hashFunction(23)], 10, 23);
-    Insert(&hashTable[hashFunction(25)], 10, 24);
-    Insert(&hashTable[hashFunction(43)], 10, 43);
-    Insert(&hashTable[hashFunction(74)], 10, 74);
-    //Insert(&hashTable[hashFunction(19)], 10, 19);
+    Insert(hashTable, 26);
+    Insert(hashTable, 30);
+    Insert(hashTable, 23);
+    Insert(hashTable, 24);
+    Insert(hashTable, 43);
+    Insert(hashTable, 74);
+    Insert(hashTable, 19);
 
-    for(int i=0; i<10; i++)
+    for (int i = 0; i < 10; i++)
     {
         printf("%d ", hashTable[i]);
     }
+
     printf("\n");
+
+    cout << "Index of 30: " << Search(hashTable, 30) << "\n";
+    cout << "Index of 74: " << Search(hashTable, 74) << "\n";
+    cout << "Index of 100: " << Search(hashTable, 100) << "\n";
+    cout << "Index of 100: " << Search2(hashTable, 100) << "\n";
 }
