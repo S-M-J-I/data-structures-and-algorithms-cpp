07b64a6657f331e18c9fd256bf36da944ffa1c2f
S-M-J-I <jishanlion@gmail.com>
2021-12-07 20:14:12 +0600

    Fixed activity selection bug

--- Algorithms/Greedy/activity_selection.cpp
+++ Algorithms/Greedy/activity_selection.cpp
@@ -1,62 +1,50 @@
 #include<bits/stdc++.h>
 #define fastio ios::sync_with_stdio(); cin.tie(0); cout.tie(0);
 
-
 using namespace std;
 
 struct Activity {
-    string name;
+    int name;
     int start, finish;
 
-    Activity(string name, int start, int finish) {
+    Activity(int name, int start, int finish) {
         this->name = name;
         this->start = start;
         this->finish = finish;
     }
 };
 
-bool compare(Activity activity1, Activity activity2) {
-    return activity1.finish < activity2.finish;
+// comapre function: sort in ascending order of finishing time
+bool compare(Activity* activity1, Activity* activity2) {
+    return activity1->finish < activity2->finish;
 }
 
-void ActivitySelctor(const vector<Activity>& times) {
 
-    vector<Activity> A;
+void ActivitySelector(vector<Activity*>& times) {
 
+    // take a list of activites
+    vector<Activity*> A;
+
+    // sort it in ascending order of finishing times
     sort(times.begin(), times.end(), compare);
 
     int n = times.size();
     int i = 0;
 
+    A.push_back(times[i]);
+    // for all activites starting from 1 to n (let the starting activity be 0)
     for(int m = 1; m < n; ++m) {
-        if(times[m].start >= times[m].finish) {
+        // if we find that the current activity's start is greater than
+        if(times[m]->start >= times[i]->finish) {
             A.push_back(times[m]);
             i = m;
         }
     }
 
-    for(Activity a : times) {
-        cout << a.name << "\n";
+    for(Activity *a : A) {
+        cout << a->name << " ";
     }
-
-}
-
-
-vector<int> ActivitySelctor(const vector<int>& start, const vector<int>& finish) {
-
-    vector<int> A = {0};
-    int n = start.size();
-    int i = 0;
-
-    for(int m = 1; m < n; ++m) {
-        if(start[m] >= finish[i]) {
-            A.push_back(m);
-            i = m;
-        }
-    }
-
-
-    return A;
+    cout << "\n";
 
 }
 
@@ -66,14 +54,14 @@ int main() {
     vector<int> start = {1, 1, 0, 4, 3, 4, 5, 2};
     vector<int> finish = {2, 3, 4, 5, 5, 6, 8, 9};
 
-    vector<int> activities = ActivitySelctor(start, finish);
+    vector<Activity*> activities;
 
-    // activity 0, 3, 6
-    for(int i : activities) {
-        cout << i << " ";
+    int n = 0;
+    for(int i = 0; i < start.size(); ++i) {
+        activities.push_back(new Activity(n++, start[i], finish[i]));
     }
-    cout << "\n";
 
+    ActivitySelector(activities);
 
     return 0;
 }
